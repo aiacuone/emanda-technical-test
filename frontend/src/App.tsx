787cdad80bef190b2cd8953344f24613a16e15e2
Adrian Iacuone <aiacuone@gmail.com>
2025-04-19 19:19:16 +1000

    header added scroll to tasks list

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,13 +5,26 @@ import { AddInput, TaskList } from './components'
 const Main = () => {
   const { addTask } = useTasks()
 
-  const onAddTask = (value: string) => addTask(value)
+  const onAddTask = (value: string) => {
+    if (!value) return alert('Please enter a task')
+    addTask(value)
+  }
 
   return (
-    <div className="flex flex-col gap-2 p-3">
-      <h1>Task Manager</h1>
-      <AddInput onAdd={onAddTask} />
-      <TaskList />
+    <div className="flex flex-col h-[100dvh]">
+      <header className="flex bg-neutral-100 p-1">
+        <h1 className="flex-1">Task Manager</h1>
+        <p className="flex gap-1">
+          <b>Adrian</b>
+          Iacuone
+        </p>
+      </header>
+      <div className="flex flex-col gap-2 p-3 flex-1 overflow-hidden">
+        <AddInput onAdd={onAddTask} />
+        <div className="flex flex-col gap-2 overflow-y-auto h-full">
+          <TaskList />
+        </div>
+      </div>
     </div>
   )
 }
